216cfb01d09f7a14b06553aaac93d0e60f15e7c5
grant smith <grantsmith1256@gmail.com>
2018-10-26 04:52:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a<=>b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  new_array = [array[0], array[2], array[1]]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$" 
+      new_array << word
+  end
+  new_array
+end
