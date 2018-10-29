7d151c88c57622ab4686d1f3ead63d2a6b01030f
grant smith <grantsmith1256@gmail.com>
2018-10-29 02:34:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,3 +32,22 @@ def kesha_maker(array)
   end
   new_array
 end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+end
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+     array.map do |word|
+       if word == array[1]
+         word
+       else word + "s"
+end
+end
+end
