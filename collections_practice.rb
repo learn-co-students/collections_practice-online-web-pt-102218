4b04bc26711a4e3fe8ade63fe1ec804ea1740afa
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-10-29 12:48:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(stuff)
+  new = stuff.sort
+  return new
+end
 
+def sort_array_desc(stuff)
+  new = stuff.sort.reverse
+end
+
+def sort_array_char_count(stuff)
+  stuff.length
+  stuff.sort
+  new = stuff
+end
